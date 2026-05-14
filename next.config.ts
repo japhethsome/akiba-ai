627b0b6f96db9eba206d16a966dbadb1742579fd
Mwaura00000 <mwaurabrian246@gmail.com>
2026-05-14 09:36:55 +0300

    ngrok tunneling

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // This helps with ngrok "Invalid Host Header" issues
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*.ngrok-free.app", "localhost:3000"]
+    }
+  }
 };
 
 export default nextConfig;
